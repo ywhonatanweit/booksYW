83045c03dfc3ac9c3b4576517237dbefac20ed06
User <User@WXI1146436>
2025-09-11 10:27:05 +0300

    Add project files.

---
+++ WpfApp2/UserControl1.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1(BookData b)
+        {
+            InitializeComponent();
+            this.BookData = b;
+            
+            this.BookName.Text=b.Title;
+            this.BookLanguage.Text = b.Language;
+            this.BookBorrow.Text = b.BorrowDate.ToString();
+            this.BookPageNum.Text = b.PageNum.ToString();
+            this.BookReturn.Text = b.ReturnDate.ToString();
+            try
+            {
+                this.BookPic.Source = new BitmapImage(new Uri(BookData.ImageURL, uriKind: UriKind.RelativeOrAbsolute) );
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Url faild");
+            }
+
+        }
+        private void OpenShowLoadData(object sender, RoutedEventArgs e)
+        {
+            
+        
+
+        }
+
+
+
+        public event EventHandler MyEvent;
+
+       public BookData BookData { get; private set; }
+            public UserControl1()
+        {
+              InitializeComponent();
+        }
+       
+    }
+}
---
+++ WpfApp2/WindowBooks.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for WindowBooks.xaml
+    /// </summary>
+    public partial class WindowBooks : Window
+    {
+       
+        public WindowBooks()
+        {
+            InitializeComponent();
+
+            List<BookData> bookdatas = new List<BookData>();
+            bookdatas.Add(item: new BookData { Title = "הארי פוטר", Language = "עברית", BorrowDate = new DateTime (22 / 10 / 2025), PageNum = 30,
+                ReturnDate = new DateTime(1 / 1 / 2026 )});
+            bookdatas.Add(item: new BookData { Title = "שר הטבעות", Language = "עברית", BorrowDate = new DateTime(28 / 8 / 2025), PageNum = 46,
+                ReturnDate = new DateTime(20/11/2025), ImageURL = "C:\\Users\\User\\source\\repos\\WpfApp2\\WpfApp2\\Pictures\\שרק הטבעות.jpg" });
+            bookdatas.Add(item: new BookData { Title = "משחקי הרעב", Language = "עברית", BorrowDate = new DateTime(11 / 5 / 2026), PageNum = 300,
+                ReturnDate = new DateTime(30 / 7 / 2026), ImageURL = "C:\\Users\\User\\source\\repos\\WpfApp2\\WpfApp2\\Pictures\\משחקי הרעב.jpg"});
+            bookdatas.Add(item: new BookData { Title = "דוד אריה", Language = "עברית", BorrowDate = new DateTime(7 / 9 / 2025), PageNum = 30, 
+                ReturnDate = new DateTime(8 / 2 / 2026), ImageURL = "C:\\Users\\User\\source\\repos\\WpfApp2\\WpfApp2\\Pictures\\דוד אריה.jpg" });
+            bookdatas.Add(item: new BookData { Title = "פרסי ג'קסון", Language = "עברית", BorrowDate = new DateTime(2 / 3 / 2026), PageNum = 30, 
+                ReturnDate = new DateTime(3 / 4 / 2026), ImageURL = "C:\\Users\\User\\source\\repos\\WpfApp2\\WpfApp2\\Pictures\\.פרסי גקשוןjpg" });
+
+            foreach (BookData book in bookdatas)
+            {
+                  UserControl1 userControl = new UserControl1(book);
+                booksp.Children.Add(userControl);
+            }
+
+
+        }
+    }
+
+
+    public class BookData
+    {
+        public string Title { get; set; }
+        public string Language { get; set; }
+        public string ImageURL { get; set; }
+        public int PageNum { get; set; }
+        public DateTime  BorrowDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+
+    }
+}
